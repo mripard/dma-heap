79d574066344eca45458652945e34eec5b3689da
Maxime Ripard <maxime@cerno.tech>
2021-07-31 19:02:56 +0200

    lib: Deduplicate global library documentation
    Signed-off-by: Maxime Ripard <maxime@cerno.tech>

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,8 @@
 // Copyright 2020-2021, Cerno
 // Licensed under the MIT License
 // See the LICENSE file or <http://opensource.org/licenses/MIT>
-//
-//! # DMA-Buf Heap Helper Library
-//!
-//! The DMA-Buf Heap interface in Linux is aimed at providing a way for the user-space to allocate
-//! memory buffers that can be efficiently shared between multiple devices through the DMA-Buf
-//! mechanism. It aims at superseeding the ION Interface previously found in Android.
-//!
-//! This library provides a safe abstraction over this interface for Rust.
-//!
-//! # Hello World
-//!
-//! ```no_run
-//! use std::fs::File;
-//! use std::os::unix::io::RawFd;
-//! use dma_heap::{DmaBufHeap, DmaBufHeapType};
-//!
-//! let heap = DmaBufHeap::new(DmaBufHeapType::Cma)
-//!     .unwrap();
-//!
-//! // Buffer will automatically be freed when `buffer_file` goes out of scope.
-//! let buffer_file: File = heap.allocate(1024).unwrap();
-//! // Buffer lifetime must be manually managed.
-//! let buffer_rawfd: RawFd = heap.allocate(1024).unwrap();
-//! ```
 
+#![doc = include_str!("../README.md")]
 #![warn(missing_debug_implementations)]
 #![warn(missing_docs)]
 #![warn(rust_2018_idioms)]
